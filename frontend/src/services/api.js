16f69e379aa3cfbea544ec7b2bee5dbfb733f0e6
Thaís Tiemi Tomiyama <t.tomiyama@unesp.br>
2025-12-03 02:44:33 -0300

    Update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+  import.meta.env.VITE_API_BASE_URL || "https://i-collect-mongodb.vercel.app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
